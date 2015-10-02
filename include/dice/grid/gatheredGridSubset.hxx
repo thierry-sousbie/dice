b5c22a9928a2b190c87b24b404237257f5e7c8c4
Thierry Sousbie <tsousbie@gmail.com>
2015-10-02 17:10:51 +0900

    definition of the class returned when gathering grid subsets

---
+++ include/dice/grid/gatheredGridSubset.hxx
@@ -0,0 +1,29 @@
+#ifndef GATHERED_GRID_SUBSET_HXX__
+#define GATHERED_GRID_SUBSET_HXX__
+
+#include "../dice_globals.hxx"
+
+/**
+ * @file 
+ * @brief A regular grid shared among MPI processes
+ * @author Thierry Sousbie
+ */
+
+#include "../internal/namespace.header"
+
+/** \addtogroup GRID
+ *   \{
+ */
+
+/**
+ * \class GatheredGridSubsetT
+ * \brief
+ */
+
+class GatheredGridSubsetT {
+
+};
+
+/** \}*/
+#include "../internal/namespace.footer"
+#endif
