edc52f6f88f6b1c13759faffef37ba8f07058e79
Thierry Sousbie <tsousbie@gmail.com>
2015-10-02 18:17:22 +0900

    skeleton

--- include/dice/grid/gatheredGridSubset.hxx
+++ include/dice/grid/gatheredGridSubset.hxx
@@ -20,8 +20,43 @@
  * \brief
  */
 
+template <class CC, class G>
 class GatheredGridSubsetT {
+public:
+  typedef typename G::Data Data;
+  typedef typename CC::Coord Coord;
+  friend class G;
 
+private:
+  enum Mode {indexed=0, full=1};
+  
+  explicit GatheredGridSubsetT(long count, int mode[], int nReceive[])
+  {
+    this->count=count;
+    indexedStart.assign(count+1,0);
+    fullStart.assign(count+1,0);
+    
+    for (int i=1;i<count+1;++i)
+      {
+	if (mode[i-1]==indexed)
+	  indexedStart[i]=nReceive[i-1];
+	else
+	  fullStart[i]=nReceive[i-1];
+
+	fullStart[i]+=fullStart[i-1];
+	indexedStart[i]+=indexedStart[i-1];
+      }
+    
+    indexedData.resize(indexedStart.back());
+    fullData.resize(fullStart.back());
+  }
+  
+  long count;
+  std::vector<Data> indexedData;
+  std::vector<Data> fullData;
+
+  std::vector<long> indexedStart;
+  std::vector<long> fullStart;
 };
 
 /** \}*/
